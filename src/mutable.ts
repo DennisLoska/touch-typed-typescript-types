5e3507a91b67754885ea660fa1e132809d7e30fd
Dennis <dennis.loska@aaron.ai>
2024-06-07 22:56:40 +0200

    ✨ feat: mutable type

---
+++ src/mutable.ts
@@ -0,0 +1,10 @@
+type Mutable<Type> = {
+  -readonly [Property in keyof Type]: Type;
+};
+
+type ImmutableObject = {
+  readonly name: number;
+  readonly age: number;
+};
+
+type MutableObject = Mutable<ImmutableObject>;
