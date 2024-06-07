4c1da81dbbc4e581bfeef9103142416c8b85636a
Dennis <dennis.loska@aaron.ai>
2024-06-07 22:57:29 +0200

    🆔 feat: stupid verbose identity type

---
+++ src/identity.ts
@@ -0,0 +1,12 @@
+// An idiot admires complexity and a genius admires simplicity. - Terry A. Davis
+// I do not admire this at all!!!
+type VerboseIdentity<Type> = {
+  [Property in keyof Type]: Type[Property];
+};
+
+type Agent = {
+  readonly name: string;
+  age?: number;
+};
+
+type Identity = VerboseIdentity<Agent>;
